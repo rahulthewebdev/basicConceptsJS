793ce5eb9c3e1e775e5aada5a6ca8a11157cdd57
RahulAditya1 <>
2021-12-20 04:57:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var eggPrice = 50;
+var onionPrice = 40;
+var isPacked = true;
+
+var deskItems =  ['laptop', 'mouse', 'keyboard', 'pen', 'paper'];
+
+// if(eggPrice < 40){
+//   if(isPacked == false){
+//     console.log('I will buy it');
+//   }else{
+//     console.log('I am not buying it');
+//   }
+// }else{
+//   console.log('price is too high');
+// }
